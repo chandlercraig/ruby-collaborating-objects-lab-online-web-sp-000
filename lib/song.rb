29d1e184225fa0f3e4f67f191b58f2a2cf0c39d8
Chandler Craig <hellochandlercraig@gmail.com>
2020-12-06 02:08:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,7 +21,7 @@ class Song
   def self.new_by_filename(filename)
     track_artist = filename.split(" - ")[0]
     track_title = filename.split(" - ")[1]
-    new_song = self.new(track_title.to_s)
+    new_song = self.new(track_title)
 
   end
 
