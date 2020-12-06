c08f46a45d2e7b7db5c8b70e054e859789bed30b
Chandler Craig <hellochandlercraig@gmail.com>
2020-12-06 00:36:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,10 +24,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(artist_name)
-    self.all.collect do |artist_instance|
-      self.new(artist_name) unless artist_instance.include?(artist_name)
-      artist_instance
-    end
+  
   end
 
   def print_songs
