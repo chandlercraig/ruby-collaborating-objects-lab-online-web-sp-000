97d36797d7c0a31a01c02c3a12d295d94ed347a0
Chandler Craig <hellochandlercraig@gmail.com>
2020-12-06 01:12:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,7 +25,7 @@ class Song
   def artist_name=(artist_name)
     our_artist = Artist.find_or_create_by_name(artist_name)
     our_artist.add_song(artist_name)
-    our_artist.name
+    
   end
 
 end
