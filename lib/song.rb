b7d4644f24bb7c14805ce121de1fc7666ef34186
Chandler Craig <hellochandlercraig@gmail.com>
2020-12-06 01:19:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,9 @@ class Song
 
   def artist_name=(artist_name)
     binding.pry
-    Artist.find_or_create_by_name(artist_name)
+    our_artist = Artist.find_or_create_by_name(artist_name)
+    artist_instance = Artist.all.find {|artist_instance| artist_instance.name == artist_name}
+    artist_instance.add_song(artist_name)
 
   end
 
